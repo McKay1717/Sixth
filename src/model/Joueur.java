f0299309d044c4be5d16adc479f1a8947d4d2c31
McKay1717 <nicolas1706ni@gmail.com>
2016-11-25 14:48:28 +0100

    Ajout de joueur

---
+++ src/model/Joueur.java
@@ -0,0 +1,75 @@
+package model;
+
+public class Joueur {
+	private String nom ="";
+	private int color = Pion.SANS_COULEUR; // -1 Rouge -2 Blanc
+	private int score;
+	public static final int MAX_PION = 15;
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * @return the color
+	 */
+	public int getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(int color) {
+		this.color = color;
+	}
+	/**
+	 * @return the score
+	 */
+	public int getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(int score) {
+		this.score = score;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Joueur)) {
+			return false;
+		}
+		Joueur other = (Joueur) obj;
+		if (color != other.color) {
+			return false;
+		}
+		if (nom == null) {
+			if (other.nom != null) {
+				return false;
+			}
+		} else if (!nom.equals(other.nom)) {
+			return false;
+		}
+		if (score != other.score) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ test/model/JoueurUnitTest.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author nicolas
+ *
+ */
+public class JoueurUnitTest {
+
+
+
+	/**
+	 * Test method for {@link model.Joueur#getNom()}.
+	 */
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#setNom(java.lang.String)}.
+	 */
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#getColor()}.
+	 */
+	@Test
+	public void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#setColor(int)}.
+	 */
+	@Test
+	public void testSetColor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#getScore()}.
+	 */
+	@Test
+	public void testGetScore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#setScore(int)}.
+	 */
+	@Test
+	public void testSetScore() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link model.Joueur#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}

03d9be1f5b173c9aa60d8fff5536f328464edad0
Raphael-R-R <raphael.ragoomundun@openmailbox.org>
2016-11-25 09:22:52 +0100

    Déplacements d'une pièce sur la grille, testDeplacerPiece() est à compléter

--- test/model/GrilleUnitTest.java
+++ test/model/GrilleUnitTest.java
@@ -16,14 +16,33 @@ public class GrilleUnitTest {
         Assert.assertNotEquals(grille.getCase(2, 4), null);
     }
 
-    // TODO : A compléter
+    /* Note : A compléter + on peut pas tester toutes les possibilités,
+     *        je crois qu'il va falloir utiliser des mocks
+     */
+
     @Test
-    public void testDeplacerPiece() {
+    public void testDeplacerPiecePion() {
         Grille grille = new Grille();
 
         grille.poserPiece(2, 3);
-        grille.deplacerPiece(2, 3, 1, 3);
 
+        // Déplacements diagonale sur case vide
+
+        grille.deplacerPiece(2, 3, 1, 2);
+        Assert.assertEquals(grille.getCase(1, 2), null);
+
+        grille.deplacerPiece(2, 3, 1, 4);
+        Assert.assertEquals(grille.getCase(1, 4), null);
+
+        grille.deplacerPiece(2, 3, 3, 4);
+        Assert.assertEquals(grille.getCase(3, 4), null);
+
+        grille.deplacerPiece(2, 3, 3, 2);
+        Assert.assertEquals(grille.getCase(3, 2), null);
+
+        // Déplacements orthogonaux sur case vide
+
+        grille.deplacerPiece(2, 3, 1, 3);
         Assert.assertEquals(grille.getCase(1, 3), null);
     }
 }

a0b02bec31a8e61aa452ee38a1e6cf06a428af3a
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2016-12-09 10:58:08 +0100

    Classe Score ajoutée et initialisée.

---
+++ src/model/Scores.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.io.*;
+
+public class Scores {
+    public static final int NB_SCORES_SAUVEGARDES = 5;
+    public Joueur joueur;
+    public int nbCoups;
+
+    public Scores(Joueur joueur, int nbCoups) {
+        this.joueur = joueur;
+        this.nbCoups = nbCoups;
+    }
+
+    public static void saveScores(Joueur joueur, int nbCoups) throws IOException {
+        Scores scores = new Scores(joueur, nbCoups);
+        ObjectOutputStream oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(new File("Resources/scores"))));
+        oos.writeObject(scores);
+        oos.close();
+    }
+
+    public static Scores[] readScores() throws IOException, ClassNotFoundException {
+        Scores[] scores = new Scores[NB_SCORES_SAUVEGARDES];
+        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(new File("Ressources/scores"))));
+        for (int i = 0; i < NB_SCORES_SAUVEGARDES; i++)
+            scores[i] = (Scores) ois.readObject();
+        ois.close();
+        return scores;
+    }
+
+    public String getNomJoueur() {
+        return joueur.getNom();
+    }
+
+    public int getNbCoups() {
+        return nbCoups;
+    }
+}

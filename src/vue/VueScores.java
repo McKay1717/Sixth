0942d170acab95ee65e1fcb550292ba1fb13c1e3
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2016-12-13 15:35:06 +0100

    Affichage des scores fait

--- src/vue/VueScores.java
+++ src/vue/VueScores.java
@@ -8,14 +8,26 @@ import java.util.List;
 
 import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 import static javax.swing.JOptionPane.showMessageDialog;
+import static model.Jeu.BLANC;
+import static model.Jeu.ROUGE;
 import static model.Scores.readScores;
 
 public class VueScores {
     public static void afficheScores(JFrame jFrame) throws IOException, ClassNotFoundException {
         List<Scores> scores = readScores();
         String message = "";
-        for (Scores score : scores)
-            message += score.getNomJoueur() + " — " + score.getCouleur() + " — " + score.getNbCoups();
+        for (Scores score : scores) {
+            message += score.getNomJoueur() + " — ";
+            switch (score.getCouleur()) {
+                case ROUGE:
+                    message += " ROUGE";
+                    break;
+                case BLANC:
+                    message += "BLANC";
+                    break;
+            }
+            message += " — " + score.getNbCoups() + "\n";
+        }
         showMessageDialog(jFrame, message, "Meilleurs scores", INFORMATION_MESSAGE);
     }
 }

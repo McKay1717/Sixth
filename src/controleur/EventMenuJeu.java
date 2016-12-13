ac01d532f7a42ebefd4d76b165525d7997a8548a
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2016-12-13 14:20:43 +0100

    Bouton Quitter du menu

--- src/controleur/EventMenuJeu.java
+++ src/controleur/EventMenuJeu.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import static java.lang.System.exit;
+
 public class EventMenuJeu implements ActionListener {
     private ControlleurGeneral controlleurGeneral;
     private MenuJeu menuJeu;
@@ -26,7 +28,8 @@ public class EventMenuJeu implements ActionListener {
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
-        }
+        } else if (((JMenuItem) e.getSource()).getText().equals("Quitter"))
+            exit(0);
     }
 
     public void accueil() throws IOException {

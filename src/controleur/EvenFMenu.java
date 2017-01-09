6419e2c4e4111c392fc9fd4d7015f0662476b64b
Florent8 <florent.martin02@edu.univ-fcomte.fr>
2017-01-09 21:41:58 +0100

    Pseudonymes non nuls && différents

--- src/controleur/EvenFMenu.java
+++ src/controleur/EvenFMenu.java
@@ -5,8 +5,7 @@ import vue.FenetreMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import static javax.swing.JOptionPane.QUESTION_MESSAGE;
-import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.*;
 
 public class EvenFMenu implements ActionListener {
 
@@ -22,7 +21,19 @@ public class EvenFMenu implements ActionListener {
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getSource().equals(fenetreMenu.getbPLay())) {
             String joueur_rouge = showInputDialog(fenetreMenu, "Entrez le nom du joueur rouge.", "Noms des joueurs", QUESTION_MESSAGE);
+            while (joueur_rouge.equals("")) {
+                showMessageDialog(fenetreMenu, "Vous n'avez pas entré de pseudonyme.", "Noms des joueurs", INFORMATION_MESSAGE);
+                joueur_rouge = showInputDialog(fenetreMenu, "Entrez le nom du joueur rouge.", "Noms des joueurs", QUESTION_MESSAGE);
+            }
             String joueur_blanc = showInputDialog(fenetreMenu, "Entrez le nom du joueur blanc", "Noms des joueurs", QUESTION_MESSAGE);
+            while (joueur_blanc.equals("")) {
+                showMessageDialog(fenetreMenu, "Vous n'avez pas entré de pseudonyme.", "Noms des joueurs", INFORMATION_MESSAGE);
+                joueur_blanc = showInputDialog(fenetreMenu, "Entrez le nom du joueur blanc", "Noms des joueurs", QUESTION_MESSAGE);
+            }
+            while (joueur_blanc.equals(joueur_rouge)) {
+                showMessageDialog(fenetreMenu, "Vous avez entré le même pseudonyme que pour le joueur rouge.", "Noms des joueurs", INFORMATION_MESSAGE);
+                joueur_blanc = showInputDialog(fenetreMenu, "Entrez le nom du joueur blanc", "Noms des joueurs", QUESTION_MESSAGE);
+            }
             controlleurGeneral.createFenetreGrille(joueur_rouge, joueur_blanc);
         } else if (actionEvent.getSource().equals(fenetreMenu.getbQuit())) {
             System.exit(0);
